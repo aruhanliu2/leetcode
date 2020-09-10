22b2e688a832e20f479baed5ad990b4281a244bf
aruhanliu2 <aruhanliu2-c@my.cityu.edu.hk>
2020-09-10 07:55:27 -0400

    Update and rename 1. Two Sum to 1. Two Sum.java

---
+++ 1. Two Sum.java
@@ -0,0 +1,58 @@
+/* Tags: Array, Hash Table */
+
+// Approach 1: Brute Force
+// Time: O(n^2)
+// Space: O(1)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+// Approach 2: One-pass Hash Table
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+// Approach 3: One-pass Hash Table
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+/* 这道题没有
