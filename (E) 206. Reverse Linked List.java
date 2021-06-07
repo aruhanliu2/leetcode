9235f6dee99d2d6807806935ee083896d26470cd
aruhanliu2 <aruhanliu2-c@my.cityu.edu.hk>
2021-06-07 09:25:49 -0400

    Update (E) 206. Reverse Linked List.java

--- (E) 206. Reverse Linked List.java
+++ (E) 206. Reverse Linked List.java
@@ -1,1 +1,37 @@
+/* Tags: Linked List */
+
+// Iterative
+// O(n)
+// O(1)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
+    }
+}
+
+// Recursion
+// O(n)
+// O(n)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode reversed = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return reversed;
+    }
+}
 
