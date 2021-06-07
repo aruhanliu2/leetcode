5eb86d21cea1049a2a2f4d43a4e0072494a56507
aruhanliu2 <aruhanliu2-c@my.cityu.edu.hk>
2021-06-07 11:51:30 -0400

    Update (E) 234. Palindrome Linked List.java

--- (E) 234. Palindrome Linked List.java
+++ (E) 234. Palindrome Linked List.java
@@ -37,6 +37,8 @@ class Solution {
 }
 
 // Solution 1: Copy into Array List and then Use Two Pointer Technique
+// O(n)
+// O(n)
 class Solution {
     public boolean isPalindrome(ListNode head) {
         List<Integer> vals = new ArrayList<>();
@@ -61,5 +63,64 @@ class Solution {
             back--;
         }
         return true;
+    }
+}
+
+// Solution 2: Recursive (Advanced)
+// 这个方法不是那么intuitive，比较难掌握透彻
+
+// Solution 3: Reverse Second Half In-place
+// O(n)
+// O(1) - iteratively reverse the second half
+/* The downside of this approach is that in a concurrent environment
+    (multiple threads and processes accessing the same data),
+    access to the Linked List by other threads or processes would have to be locked while this function is running,
+    because the Linked List is temporarily broken. This is a limitation of many in-place algorithms though.
+ */
+class Solution {
+
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null) return true;
+
+        // Find the end of first half and reverse second half.
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // Check whether or not there is a palindrome.
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }        
+        // Restore the list and return the result.
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    // Taken from https://leetcode.com/problems/reverse-linked-list/solution/
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
