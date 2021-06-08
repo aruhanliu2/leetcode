5ed7b79f1bba810ade95470a8079d70478047481
aruhanliu2 <aruhanliu2-c@my.cityu.edu.hk>
2021-06-07 21:16:15 -0400

    Update (E) 349. Intersection of Two Arrays.java

--- (E) 349. Intersection of Two Arrays.java
+++ (E) 349. Intersection of Two Arrays.java
@@ -1,6 +1,8 @@
 /* Tags: Hash Table, Two Pointers, Binary Search, Sort */
+
+// My solution: hash table
 // O(m+n)
-// O(m)
+// O(m) - worst case
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         if(nums1 == null || nums2 == null) {
@@ -22,5 +24,109 @@ class Solution {
             result[count++] = e;
         }
         return result;
+    }
+}
+
+// My solution: two pointers + sorting
+// O(nlogn + mlogm)
+// O(min(m,n)) - worst case
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums2 == null) {
+            return new int[0];
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        List<Integer> list = new ArrayList<>();
+        while(i < nums1.length && j < nums2.length) {
+            while(i + 1 < nums1.length && nums1[i] == nums1[i+1]) {
+                i++;
+            }
+            while(j + 1 < nums2.length && nums2[j] == nums2[j+1]) {
+                j++;
+            }
+            if(nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++; //易错点:非常容易忘记increment pointer
+                j++;
+            } else if(nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        int[] result = new int[list.size()];
+        int index = 0;
+        for(int e : list) {
+            result[index++] = e;
+        }
+        return result;
+    }
+}
+
+//优化第二种解法：通过HashSet去重
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
+    }
+}
+
+//Binary search
+// O(mlogn + nlogn)
+// O(min(m,n)) - worst case
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+    
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
     }
 }
