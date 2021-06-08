f3a5c98bfd911ebfec8daadfa7bbb1439f828125
aruhanliu2 <aruhanliu2-c@my.cityu.edu.hk>
2021-06-07 20:12:23 -0400

    Update (E) 203. Remove Linked List Elements.java

--- (E) 203. Remove Linked List Elements.java
+++ (E) 203. Remove Linked List Elements.java
@@ -1,3 +1,7 @@
+/* Tags: Linked List */
+// My solution
+// O(n)
+// O(1)
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummyNode = new ListNode();
@@ -15,3 +19,12 @@ class Solution {
         return dummyNode.next;
     }
 }
+/*
+    Sentinel nodes are widely used in trees and linked lists as pseudo-heads, pseudo-tails, markers of level end, etc.
+    They are purely functional, and usually does not hold any data.
+    Their main purpose is to standardize the situation,
+    for example, make linked list to be never empty and never headless and hence simplify insert and delete.
+    Here are two standard examples:
+    - LRU Cache, sentinel nodes are used as pseudo-head and pseudo-tail.
+    - Tree Level Order Traversal, sentinel nodes are used to mark level end.
+*/
